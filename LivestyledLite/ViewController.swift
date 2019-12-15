05139c0b281dc7b470e2046c7057e55a249c324b
zip520123 <zip520123@yahoo.com.tw>
2019-12-15 15:41:46 +0000

    Initial Commit

---
+++ LivestyledLite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LivestyledLite
+//
+//  Created by zip520123 on 2019/12/15.
+//  Copyright © 2019年 zip520123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
